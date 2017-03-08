f168d5fd532fcfa928b843dbd15791662d18cdde
tdao3 <tdao3@uic.edu>
2017-03-07 21:54:32 -0600

    OKRs and 2 beginning scripts

---
+++ Movement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+public class Movement : MonoBehaviour {
+
+    Animator anime;
+    public static int health = 100;
+    public Slider healthBar;
+
+    bool underCover = false;
+    bool isWalking = false;
+
+    void Awake() {
+        anime = GetComponent<Animator>();
+    }
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("ReduceHealth", 1, 1);
+	}
+	
+	void Update () {
+
+        Turning();
+        Walking();
+        Move();
+        Jump();
+    }
+
+    private void ReduceHealth() {
+        health = health - 20;
+        healthBar.value = health;
+        if (health <= 0) {
+            anime.SetTrigger("Death");
+        }
+    }
+
+    private void Turning() {
+        anime.SetFloat("Turn", Input.GetAxis("Horizontal"));
+    }
+
+    private void Jump() {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            anime.SetTrigger("Jump");
+        }
+    }
+
+    private void Walking() {
+        if (Input.GetKeyDown(KeyCode.LeftShift)) {
+            isWalking =! isWalking;
+            anime.SetBool("Walk", isWalking);
+        }
+    }
+    private void Move() {
+        anime.SetFloat("Forward", Input.GetAxis("Vertical"));
+    }
+
+
+
+}
